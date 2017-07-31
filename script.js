f2863c6c6edee48c30b2554e76452d7f81932806
Ami Schroeder <arschroeder22@gmail.com>
2017-07-31 14:02:20 -0500

    when click, change background to yellow

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ $(document).ready(function(){
     $('body').append('<p><button class="generateButton">Generate</button></p>');
     $('body').on('click', '.generateButton', function(){
         buttonClicks++;
-        $('body').append('<div><p>' + buttonClicks + '<button>Swap</button>' + '<button id="deleteButton">Delete</button>' + '</p></div>');
+        $('body').append('<div><p>' + buttonClicks + '<button id="swapButton">Swap</button>' + '<button id="deleteButton">Delete</button>' + '</p></div>');
         $('div').on('click',  '#deleteButton', function(){
             $(this).parent().remove();
-        })
+        });
+        $('div').on('click', '#swapButton', function(){
+            $(this).parent().css("background-color", "yellow");
+        });
     });
 });
