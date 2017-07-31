3ff09b9895a00e07035ce1617a9769ee8baf994b
Ami Schroeder <arschroeder22@gmail.com>
2017-07-31 13:56:36 -0500

    make delete button delete line

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
     $('body').append('<p><button class="generateButton">Generate</button></p>');
     $('body').on('click', '.generateButton', function(){
         buttonClicks++;
-        $('body').append('<div><p>' + buttonClicks + '<button>Swap</button>' + '<button>Delete</button>' + '</p></div>');
+        $('body').append('<div><p>' + buttonClicks + '<button>Swap</button>' + '<button id="deleteButton">Delete</button>' + '</p></div>');
+        $('div').on('click',  '#deleteButton', function(){
+            $(this).parent().remove();
+        })
     });
 });
